e2529ac858dcb26b8df99a4fad9cd2227606467a
viktor-punko <punko.viktor@yandex.ru>
2014-07-10 15:14:20 +0300

    fix bug: href=""

--- number.js
+++ number.js
@@ -16,6 +16,21 @@ function NumberOperations () {
 		return result;
 	}
 
+	this.minus = function (a, b) {
+		var result;
+		var abkey = a + '-' + b;
+		if (this.isABPairContainCache(a,b, "-") == true){
+			result = cache[abkey];
+		}
+		else
+		{
+			result = a - b;
+			//save in cache
+			cache[abkey] = result;
+		}
+		return result;
+	}
+
 	this.isABPairContainCache = function(a, b, op){
 		var result = false;
 		var abkeys = [a + op + b, b + op + a];
